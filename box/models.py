f8ed0154739f7ff0e7ff54297531873bfad216ec
Alex Abell <aga8@duke.edu>
2017-04-16 13:18:49 -0400

    added random string

--- box/models.py
+++ box/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+import random
+import string
 # Create your models here.
 
 def get_upload_path(instance, filename):
     #print(instance.user.username)
-    return './{0}/{1}'.format(instance.user.username, filename)
+    #return './{0}/{1}'.format(instance.user.username, filename)
+    r = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+
+    return './{0}/'.format(instance.user.username)+r+'/{0}'.format(filename)
 
 class FileModel(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
